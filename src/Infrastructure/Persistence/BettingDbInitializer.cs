4737da99e4ff0c194f4917828595a6906b7c6a6b
Kalin Tsenkov <kalintsenkov3@gmail.com>
2020-11-25 19:26:27 +0200

    Added initial matches data

--- src/Infrastructure/Persistence/BettingDbInitializer.cs
+++ src/Infrastructure/Persistence/BettingDbInitializer.cs
@@ -1,6 +1,11 @@
 ﻿namespace BettingSystem.Infrastructure.Persistence
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
     using System.Threading.Tasks;
+    using Domain.Common;
     using Identity;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
@@ -12,15 +17,18 @@
         private readonly BettingDbContext db;
         private readonly UserManager<User> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IEnumerable<IInitialData> initialDataProviders;
 
         public BettingDbInitializer(
             BettingDbContext db,
             UserManager<User> userManager,
-            RoleManager<IdentityRole> roleManager)
+            RoleManager<IdentityRole> roleManager,
+            IEnumerable<IInitialData> initialDataProviders)
         {
             this.db = db;
             this.userManager = userManager;
             this.roleManager = roleManager;
+            this.initialDataProviders = initialDataProviders;
         }
 
         public void Initialize()
@@ -28,6 +36,21 @@
             this.db.Database.Migrate();
 
             this.SeedAdministrator();
+
+            foreach (var initialDataProvider in this.initialDataProviders)
+            {
+                if (this.DataSetIsEmpty(initialDataProvider.EntityType))
+                {
+                    var data = initialDataProvider.GetData();
+
+                    foreach (var entity in data)
+                    {
+                        this.db.Add(entity);
+                    }
+                }
+            }
+
+            this.db.SaveChanges();
         }
 
         private void SeedAdministrator()
@@ -52,5 +75,27 @@
                 })
                 .GetAwaiter()
                 .GetResult();
+
+        private bool DataSetIsEmpty(Type type)
+        {
+            var setMethod = this.GetType()
+                    .GetMethod(nameof(this.GetSet), BindingFlags.Instance | BindingFlags.NonPublic)!
+                .MakeGenericMethod(type);
+
+            var set = setMethod.Invoke(this, Array.Empty<object>());
+
+            var countMethod = typeof(Queryable)
+                .GetMethods()
+                .First(m => m.Name == nameof(Queryable.Count) && m.GetParameters().Length == 1)
+                .MakeGenericMethod(type);
+
+            var result = (int)countMethod.Invoke(null, new[] { set })!;
+
+            return result == 0;
+        }
+
+        private DbSet<TEntity> GetSet<TEntity>()
+            where TEntity : class
+            => this.db.Set<TEntity>();
     }
 }
