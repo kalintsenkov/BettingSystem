3729938ea7ff334de055ec403d100e36943b5278
Kalin Tsenkov <kalintsenkov3@gmail.com>
2021-07-18 19:47:10 +0300

    Added tests in Teams

--- src/Server/Teams/Teams.Domain/Models/Team.Specs.cs
+++ src/Server/Teams/Teams.Domain/Models/Team.Specs.cs
@@ -26,5 +26,45 @@
 
             act.Should().Throw<InvalidTeamException>();
         }
+
+        [Theory]
+        [InlineData("Test 1")]
+        [InlineData("Test 2")]
+        [InlineData("Test 3")]
+        public void UpdateNameShouldSetNewNameIfNameIsValid(string newName)
+        {
+            var team = A.Dummy<Team>();
+
+            team.UpdateName(newName);
+
+            team.Name.Should().Be(newName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("cs")]
+        [InlineData(null)]
+        public void UpdateNameShouldThrowExceptionIfNameIsInvalid(string newName)
+        {
+            var team = A.Dummy<Team>();
+
+            Action act = () => team.UpdateName(newName);
+
+            act.Should().Throw<InvalidTeamException>();
+        }
+
+        [Theory]
+        [InlineData("Test 1")]
+        [InlineData("Test 2")]
+        [InlineData("Test 3")]
+        public void AddPlayerShouldWork(string playerName)
+        {
+            var team = A.Dummy<Team>();
+
+            team.AddPlayer(playerName, Position.Forward);
+
+            team.Players.Count.Should().Be(1);
+            team.Players.Should().ContainSingle(p => p.Name == playerName);
+        }
     }
 }
